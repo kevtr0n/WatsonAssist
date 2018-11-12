a208492f12fc4a37619215738140a89c6966757e
Kevin Hayden <kevphayden@gmail.com>
2018-11-12 16:53:51 -0500

    Actually adding the store.

--- src/store.ts
+++ src/store.ts
@@ -5,6 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // Booleans to check tone state:
+    isAnalytical: false,
+    isConfident: false,
+    isTentative: false,
+    isAnger: false,
+    isFear: false,
+    isJoy: false,
+    
+    // Tone state percentage values:
+    analytical: 0.0,
+    confident: 0.0,
+    tentative: 0.0,
+    anger: 0.0,
+    fear: 0.0,
+    joy: 0.0
+  },
+  getters: {
 
   },
   mutations: {
